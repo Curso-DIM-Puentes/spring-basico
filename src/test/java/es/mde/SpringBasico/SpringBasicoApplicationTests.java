b20433495c10bf46b36a67a6089d7c19ad13caae
joseluispuentes82 <joseluispuentes@hotmail.com>
2024-03-04 18:33:39 +0100

    Proyecto creado

---
+++ src/test/java/es/mde/SpringBasico/SpringBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.SpringBasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
